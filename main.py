89c61d6d76cbed27d5717e7171abeeb16ba2f7b1
THE-NIKHIL07 <nikhilbisht058@gmail.com>
2025-10-21 20:09:23 +0530

    completed data transformation

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import sys
 
-from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.components.data_validation import DataValidation
 
-from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig
+
 if __name__=='__main__':
     try :
-        trainingpipelineconfig=TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion=DataIngestion(dataingestionconfig)
+        training_pipeline_config=TrainingPipelineConfig()
+
+        data_ingestion_config=DataIngestionConfig(training_pipeline_config)
+        data_ingestion=DataIngestion(data_ingestion_config)
+
         logging.info("Initiate the data ingestion")
-        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation completed")
-        print(dataingestionartifact)
-        datavalidationconfig=DataValidationConfig(trainingpipelineconfig)
+        print(data_ingestion_artifact)
+
+        datavalidationconfig=DataValidationConfig(training_pipeline_config)
         logging.info("Initiate the data validation")
-        data_validation=DataValidation(dataingestionartifact,datavalidationconfig)
+        data_validation=DataValidation(data_ingestion_artifact,datavalidationconfig)
         logging.info("Data Validation completed")
         data_validation_artifact=data_validation.initiate_data_validation()
         print(data_validation_artifact)
 
+        data_transformation_config=DataTransformationConfig(training_pipeline_config)
+        logging.info("Data Transformation  Started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformartion()
+        print(data_transformation_artifact)
+        logging.info("data tarnsformation completed")
 
 
     except Exception as e:
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -24,4 +24,24 @@ def write_yaml_file(file_path:str,content:object,replace:bool=False)->None:
                   with open(file_path,"w") as file :
                        yaml.dump(content,file)
         except Exception as e:
-             raise NetworkSecurityException(e,sys)                    
+             raise NetworkSecurityException(e,sys) 
+
+def save_numpy_array_data(filepath:str,array:np.array):
+     try:
+          dir_path=os.path.dirname(filepath)
+          os.makedirs(dir_path,exist_ok=True)
+
+          with open(filepath,"wb") as file :
+                np.save(file,array)
+     except Exception as e :
+          raise NetworkSecurityException(e,sys)           
+
+def save_object(file_path:str,obj:object)->None :
+     try:
+          logging.info("Entered the save_object method of MainUtils class")
+          os.makedirs(os.path.dirname(file_path),exist_ok=True)
+          with open(file_path,"wb") as file :
+               pickle.dump(obj,file)
+          logging.info("Exited the save_ovject method of mainutils class")
+     except Exception as e :                                       
+        raise NetworkSecurityException(e,sys)
